2117b5addcc50273f169fefcef70fdcc822b24df
sorki <srk@48.io>
2024-04-06 19:57:14 +0200

    pft

--- src/Network/CANOpen.hs
+++ src/Network/CANOpen.hs
@@ -10,43 +10,52 @@ import Control.Monad.Trans.Reader (ReaderT, runReaderT)
 import Data.Map (Map)
 import Network.CAN (CANArbitrationField, CANMessage)
 import Network.CANOpen.Types (NodeID)
-import Network.CANOpen.NMT.Types (NMTState)
+import Network.CANOpen.NMT.Types (NMTState(..))
 
-import Control.Concurrent (ThreadId)
-import Control.Concurrent.STM (TVar)
+--import Control.Concurrent (ThreadId)
+import Control.Concurrent.STM -- (TVar, TQueue)
+
+import qualified Data.Map
+import qualified Network.CANOpen.NMT.Types
 
 data Node = Node
   { nodeNMTState :: TVar NMTState
   --, nodeThreads :: [ThreadId]
   }
 
-data CANOpenState m = CANOpenState
-  { canOpenStateNodes :: Map NodeID Node
-  , canOpenStateHandlers :: Map CANArbitrationField (CANMessage -> m ())
+data CANOpenState = CANOpenState
+  { canOpenStateNodes :: TVar (Map NodeID Node)
+  , canOpenStateHandlers :: TVar (Map CANArbitrationField (CANMessage -> IO ()))
+  , canOpenStateInbound :: TQueue CANMessage
+  , canOpenStateOutbound :: TQueue CANMessage
   }
 
-class MonadCANOpen m where
-  addNode
-    :: NodeID
-    -> m Node
-
-setOperational = undefined
-
-sdoWrite = undefined
-sdoRead = undefined
+{--
+newCANOpenState
+  :: TQueue CANMessage -- ^ In
+  -> TQueue CANMessage -- ^ Out
+  -> CANOpenState
+newCANOpenState inQ outQ =
+  CANOpenState
+  { canOpenStateNodes = mempty
+  , canOpenStateHandlers = mempty
+  , canOpenStateInbound = inQ
+  , canOpenStateOutbound = outQ
+  }
+--}
 
 data CANOpenError
 
 newtype CANOpenT m a = CANOpenT
   { _unCANOpenT
       :: ExceptT CANOpenError
-          (ReaderT (CANOpenState m) m) a
+          (ReaderT CANOpenState m) a
   }
   deriving
     ( Functor
     , Applicative
     , Monad
-    , MonadReader (CANOpenState m)
+    , MonadReader CANOpenState
     , MonadError CANOpenError
     , MonadIO
      )
@@ -57,10 +66,54 @@ instance MonadTrans CANOpenT where
 -- | Run CANOpenT transformer
 runCANOpenT
   :: Monad m
-  => CANOpenState m
+  => CANOpenState
   -> CANOpenT m a
   -> m (Either CANOpenError a)
 runCANOpenT s =
     (`runReaderT` s)
   . runExceptT
   . _unCANOpenT
+
+class MonadCANOpen m where
+  addNode
+    :: NodeID
+    -> m Node
+
+  registerHandler
+    :: CANArbitrationField
+    -> (CANMessage -> IO ())
+    -> m ()
+
+--setOperational = undefined
+--sdoWrite = undefined
+--sdoRead = undefined
+
+instance MonadIO m => MonadCANOpen (CANOpenT m) where
+  addNode nID = do
+    nState <- liftIO $ newTVarIO NMTState_Initialising
+    let
+      newNode =
+        Node
+        { nodeNMTState = nState
+        }
+
+    ts <- ask
+    liftIO
+      $ atomically
+      $ modifyTVar
+          (canOpenStateNodes ts)
+          (Data.Map.insert nID newNode)
+
+    registerHandler
+      (Network.CANOpen.NMT.Types.nodeHeartbeatID nID)
+      (liftIO . print)
+
+    pure newNode
+
+  registerHandler cID handler = do
+    ts <- ask
+    liftIO
+      $ atomically
+      $ modifyTVar
+          (canOpenStateHandlers ts)
+          (Data.Map.insert cID handler)
