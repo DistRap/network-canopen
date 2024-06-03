bc6e024d8011b56eb3778e682e1847cbc2f86075
sorki <srk@48.io>
2024-06-03 18:58:15 +0200

    test cleanup

--- src/Network/CANOpen/Test.hs
+++ src/Network/CANOpen/Test.hs
@@ -1,6 +1,6 @@
 module Network.CANOpen.Test where
 
-import Control.Concurrent
+--import Control.Concurrent
 import Control.Monad
 import Control.Monad.IO.Class (MonadIO(liftIO))
 
@@ -8,24 +8,22 @@ import Control.Monad.IO.Class (MonadIO(liftIO))
 import Data.Word (Word8, Word32)
 import Data.Int (Int32)
 
-import Network.CAN
 import Network.CANOpen
 import Network.CANOpen.Serialize (CSerialize(..))
 import Network.CANOpen.Types
 import Network.CANOpen.LSS
-import Network.CANOpen.LSS.Types
+--import Network.CANOpen.LSS.Types
 import Network.CANOpen.SDO
-import qualified Network.SLCAN
+--import qualified Network.SLCAN
 import Network.SocketCAN
 import qualified Network.CANOpen.Serialize
 import qualified Network.CANOpen.SubBus
 
-import qualified System.IO
+--import qualified System.IO
 
 import Control.Concurrent
-import Control.Concurrent.STM
+import Control.Concurrent.STM (atomically, orElse)
 import Control.Concurrent.STM.TMVar
-import qualified Data.Map
 import UnliftIO.Async (Async)
 import qualified UnliftIO.Async
 
@@ -78,8 +76,6 @@ sdoRead
      , MonadIO m
      )
   => SDOClient
---  -> TMVar SDOClientCommand
---  -> TMVar SDOClientReply
   -> NodeID
   -> Variable a
   -> m a
@@ -109,8 +105,6 @@ sdoWrite
      , MonadIO m
      )
   => SDOClient
---  => TMVar SDOClientCommand
---  -> TMVar SDOClientReply
   -> NodeID
   -> Variable a
   -> a
@@ -169,7 +163,7 @@ main = do
 
   --System.IO.withFile "/dev/can4discouart" System.IO.ReadWriteMode $ \h -> Network.SLCAN.runSLCAN h def $ do
   runSocketCAN "vcan0" $ do
-    runCANOpen $ do
+    void $ runCANOpen $ do
       --forever $ do
       --  switchModeGlobal LSSMode_Operation
       --  recv >>= l
@@ -189,17 +183,6 @@ main = do
 
       -- UnliftIO.Async.async $ do
       Network.CANOpen.SubBus.runSubBus tb $ do
-          {--
-          -- read vendor id
-          raw <- sdoClientUpload nID (Mux 0x1018 1) 
-          -- >>= l
-          let
-            des =
-              either (error "DesFail") id
-              $ Network.CANOpen.Serialize.runGet raw
-          l (des :: Word32)
-          --}
-
           forever $ do
             cmd <-
               liftIO
@@ -234,13 +217,3 @@ main = do
                   . atomically
                   $ takeTMVar
                       (sdoClientDownload' sdoClient)
-
-          {--
-          -- at 0x606C $ field "velocity_actual" sint32 & ro
-          sdoClientUpload @Int32 nID (Mux 0x606C 0) >>= l
-          -- at 0x60FF $ field "target_velocity" sint32
-          sdoClientDownload @Int32 nID (Mux 0x60FF 0) (123242)
-          --}
-
-      pure ()
-  >>= print
